89f6253958d01f713afe693301fa61b3de40d39b
Ricky <ricky@Rikys-MacBook-Pro.local>
2021-08-24 10:55:39 -0600

    C

--- src/public/js/getInfoHelper.js
+++ src/public/js/getInfoHelper.js
@@ -3,26 +3,30 @@ const puppeteer = require('puppeteer-extra')
 
 async function getInfoHelper(page,reviews,websites){
 
-     
+     try{
+          let numberOfReviews = await page.$eval('.css-1h1j0y3',review => {
+            if(!review.innerText.includes('reviews'))  return false                                                         
+              return review.innerText
+          })
 
-      let numberOfReviews = await page.$eval('.css-1h1j0y3',review => {
-        if(!review.innerText.includes('reviews'))  return false                                                         
-          return review.innerText
-      })
+          // if(numberOfReviews === false) {
+          //   numberOfReviews = await page.$eval('.css-bq71j2',review => {
+          //     if(!review.innerText.includes('reviews'))  return 'No Reviews'                                                         
+          //       return review.innerText
+          //   })
+          // }
+    
+          reviews.push(numberOfReviews)
+          let website = await page.$eval('p.css-1h1j0y3 a.css-ac8spe',website => {
+            if(website.innerText.includes('Get Directions'))  return 'No website'                                                           
+                return website.innerText
+          })
+          websites.push(website)
 
-      if(numberOfReviews === false) {
-        numberOfReviews = await page.$eval('.css-bq71j2',review => {
-          if(!review.innerText.includes('reviews'))  return 'No Reviews'                                                         
-            return review.innerText
-        })
-      }
+    }catch(e){
 
-      reviews.push(numberOfReviews)
-      let website = await page.$eval('p.css-1h1j0y3 a.css-ac8spe',website => {
-        if(website.innerText.includes('Get Directions'))  return 'No website'                                                           
-            return website.innerText
-      })
-      websites.push(website)
+
+    }
 
 }
 
